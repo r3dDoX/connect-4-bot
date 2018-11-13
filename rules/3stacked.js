1b3d68e70ec8618ac7afa1037bd45d1259239f37
Matthias Baumeler <matthias.baumeler@gmail.com>
2018-11-13 20:40:38 +0100

    3 Stack strategy.

--- rules/3stacked.js
+++ rules/3stacked.js
@@ -1,3 +1,23 @@
-module.exports = function(board, playerColor) {
+module.exports = function (board, playerColor) {
 
+    board[0].map((row, columnIndex) => board
+        .map(rows => rows[columnIndex]))
+        .map((cells, index) => ({index, cells}))
+        .filter(({cells}) => cells[0] === "EMPTY")
+        .find(indexCellObject => {
+            let chips = indexCellObject.cells.filter(cell => cell !== "EMPTY");
+            if (chips.length === 0) {
+                return false;
+            }
+            let color = chips[0];
+            let other = false;
+            return chips.filter(chip => {
+                    if (chip === color && other === false) {
+                        return true;
+                    }
+                    other = true;
+                    return false
+                }
+            ).length === 3;
+        });
 }
